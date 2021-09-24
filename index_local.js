732978c0d0ead5640baf0d2328d7eaf4f11e9bb2
Miura55 <acordion.piano@gmail.com>
2021-09-25 00:00:52 +0900

    環境変数設定ファイルを追加

--- index_local.js
+++ index_local.js
@@ -1,8 +1,9 @@
-
 const Line = require('./line');
 const myLine = new Line();
 
+// 環境変数の読み込み
+require('dotenv').config();
 // LINE Notify トークンセット
-myLine.setToken(process.env.LINE_TOKEN);
+myLine.setToken(process.env.LINE_NOTIFY_ACCESS_TOKEN);
 // LINE Notify 実行（「こんにちは！」とメッセージを送る）
 myLine.notify('Local Test LINE Notify AWS CloudTech');
