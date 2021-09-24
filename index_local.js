55971c8bb74a5544d5aa78af42f365bd77a454af
Miura55 <acordion.piano@gmail.com>
2021-09-25 02:51:23 +0900

    RSSの通知のコードの環境変数化

--- index_local.js
+++ index_local.js
@@ -1,8 +1,8 @@
 const Line = require('./line');
 const myLine = new Line();
-
 // 環境変数の読み込み
 require('dotenv').config();
+
 // LINE Notify トークンセット
 myLine.setToken(process.env.LINE_NOTIFY_ACCESS_TOKEN);
 // LINE Notify 実行（「こんにちは！」とメッセージを送る）
